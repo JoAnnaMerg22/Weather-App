09175342d0e2a7eb0c88e1a2fa4e7ec85e408246
JoAnnaMerg22 <115831892+JoAnnaMerg22@users.noreply.github.com>
2022-12-29 17:33:14 -0500

    lesson 4 changes

--- src/app.js
+++ src/app.js
@@ -21,6 +21,36 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  let days = ["Thu", "Fri", "Sat", "Sun", "Mon"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `            
+                <div class="col-2">
+                    <div class="weather-forecast-date">${day}</div>
+                    <img
+                        src="https://openweathermap.org/img/wn/04d@2x.png"
+                        alt=""
+                        width="36"
+                        />
+                <div class="weather-forecast-temperatures">
+                    <span class="weather-forecast-temp-max">
+                        18°</span>
+                    <span class="weather-forecast-temp-min">
+                        12°</span>
+                </div>        
+                </div>
+                `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function displayTemperature(response) {
   console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
@@ -88,3 +118,4 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("New York");
+displayForecast();
